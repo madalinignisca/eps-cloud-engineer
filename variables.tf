aecbc2bf20218de5b11d5f93fa371559c3e98f72
Madalin Ignisca <git@madalin.me>
2024-03-17 02:40:49 +0100

    Remove default policy example

--- variables.tf
+++ variables.tf
@@ -5,24 +5,6 @@ variable "aws_region" {
 
 variable "aws_kms_policy" {
   description = "The policy for the KMS key"
-  default     = jsonecode(
-    {
-      Version: "2012-10-17",
-      Id: "key-consolepolicy-3",
-      Statement: [
-        {
-          Sid: "Enable IAM User Permissions",
-          Effect: "Allow",
-          Principal: {
-            "AWS": "arn:aws:iam::123456789012:root"
-          },
-          Action: "kms:*",
-          Resource: "*"
-        }
-      ]
-    }
-  
-  )
 }
 
 variable "app_api_key" {
